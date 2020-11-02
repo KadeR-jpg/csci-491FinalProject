eec7d5e68802b0a470c1c054baca0a1fb54ef137
Super-Original-Username <aswordlessninja@gmail.com>
2020-11-02 10:10:50 -0700

    added beginnings of database

--- djWebDev/storyGenerator/models.py
+++ djWebDev/storyGenerator/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    """Story genre"""
+    name = models.CharField(max_length=200, help_text='Enter a genre for your story (e.g. Fantasy)')
+
+    def __str__(self):
+        return self.name
+
+
+class BlankStory(models.Model):
+    """Model to hold information for a story template"""
+    title = models.CharField(max_length=200,help_text='Enter the title of the story', primary_key=True)
+
+    contents = models.TextField(help_text='Enter your story, using <word_type> to indicate where we should make openings in your template')
+
+    adjectiveCount = models.IntegerField()
+    nounCount = models.IntegerField()
+    adverbCount = models.IntegerField()
+    plurNounCount = models.IntegerField()
+    verbCount = models.IntegerField()
+
+    genre = models.ManyToManyField(Genre,help_text='Select a genre for the story')
+
+    #completedStories = model.ManyToManyField('WordSet')
+
+
+class WordSet(models.Model):
+    adverbs = models.TextField()
+    nouns = models.TextField()
+    plurNouns = models.TextField()
+    verbs = models.TextField()
+    adjectives = models.TextField()
+
+    story = models.ForeignKey('BlankStory',on_delete=models.SET_NULL,null=True)
