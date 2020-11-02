ca233c3919de0bc1525e215c1b9190614979c32d
Super-Original-Username <aswordlessninja@gmail.com>
2020-11-02 10:17:21 -0700

    fixed some gitignore issues

--- djWebDev/storyGenerator/admin.py
+++ djWebDev/storyGenerator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import BlankStory,Genre,WordSet
 # Register your models here.
+
+
+admin.site.register(Genre)
+admin.site.register(BlankStory)
+admin.site.register(WordSet)
