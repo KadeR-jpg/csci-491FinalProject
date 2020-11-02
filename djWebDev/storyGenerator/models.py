9a34d21ef94f62c2d8e284c8f8bf072a10993584
KadeR-jpg <54875162+KadeR-jpg@users.noreply.github.com>
2020-11-02 14:28:17 -0700

    updates

--- djWebDev/storyGenerator/models.py
+++ djWebDev/storyGenerator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    protagonist = models.CharField(max_length=30)
+    antagonist = models.CharField(max_length=30)
+    setting = models.CharField(max_length=30)
+    SETTING_CHOICES = (
+        ('spc', 'SPACE'),
+        ('wstrn', 'WESTERN'),
+        ('ar', 'ANCIENT ROME'),
+        ('dysf', 'DYSTOPIA FUTURE')
+    )
+    plot = models.CharField(max_length=30)
+    conflict = models.CharField(max_length=30)
+    genre = models.CharField(max_length=60)
