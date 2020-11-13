f4284b9e9a4e1e46b91db4492390cac9a3c71c78
Super-Original-Username <aswordlessninja@gmail.com>
2020-11-12 23:00:40 -0700

    updated submission generator to work with expected inputs from the database

--- wip_scripts/submission_generator.py
+++ wip_scripts/submission_generator.py
@@ -12,7 +12,7 @@ sample_adverbs = []
 sample_pnouns = []
 sample_pronouns = ['gremlin']
 sample_adjectives = ['small','green']
-
+full_sample = "bird,eggplant,tomato seed||||gremlin|small,green"
 
 class MLGen():
 
@@ -30,6 +30,8 @@ class MLGen():
         self.replacement_pnouns = sample_pnouns
         self.replacement_pronouns = sample_pronouns
         self.replacement_adjectives = sample_adjectives
+
+        self.safe_indices = ''
 
     # I'm pretty much using this as a switch statement to handle all of the different supported word types
     def word_handler_handler(self,w_index,word):
@@ -60,6 +62,24 @@ class MLGen():
         locals()['handle_' + word](w_index)
         
 
+    '''Takes all of the word type indices and puts them into a single string that we can use in the DB
+        I'm sure there's a better way to do that, but this will work for now. I'm no database expert'''
+    def make_DB_safe_counters(self):
+        comp_list = ""
+        comp_list += ','.join(list(map(str,self.noun_indices)))
+        comp_list += '|'
+        comp_list += ','.join(list(map(str,self.verb_indices)))
+        comp_list += '|'
+        comp_list += ','.join(list(map(str,self.adverb_indices)))
+        comp_list += '|'
+        comp_list += ','.join(list(map(str,self.pnoun_indices)))
+        comp_list += '|'
+        comp_list += ','.join(list(map(str,self.pronoun_indices)))
+        comp_list += '|'
+        comp_list += ','.join(list(map(str,self.adjective_indices)))
+        return comp_list
+
+
 
     """ Main parser functionality """
     def gen_template(self,in_template):
@@ -80,29 +100,60 @@ class MLGen():
                     #else:
                     #    print("That's not a supported word type")
 
-        for key in supported_types:
-            print(supported_types[key])
+        self.safe_indices = self.make_DB_safe_counters()
+        #for key in supported_types:
+            #print(supported_types[key])
 
-        self.pop_template(in_template,None,None)
+        #self.pop_template(in_template,None,None)
         
+    def strList_to_intList(self,l):
+        to_ret = []
+        if len(l)>0:
+            for item in l:
+                if item != '':
+                    to_ret.append(int(item))
+        return to_ret
 
 
     """Takes user input and populates a template with those responses"""
     def pop_template(self,in_template, user_responses, word_indices):
         mod_template = in_template.split()
-        for ind in range(len(self.noun_indices)):
-            mod_template[self.noun_indices[ind]] = re.sub(tag_re,self.replacement_nouns[ind],mod_template[self.noun_indices[ind]])
-        for ind in range(len(self.verb_indices)):
-            mod_template[self.verb_indices[ind]] = re.sub(tag_re,self.replacement_verbs[ind],mod_template[self.verb_indices[ind]])
+        # splits the word index array into arrays per word type
+        word_indices = word_indices.split('|')
+        word_indices = [i.split(',') for i in word_indices]
+        noun_indices = self.strList_to_intList(word_indices[0])
+        verb_indices = self.strList_to_intList(word_indices[1])
+        adverb_indices = self.strList_to_intList(word_indices[2])
+        pnoun_indices = self.strList_to_intList(word_indices[3])
+        pronoun_indices = self.strList_to_intList(word_indices[4])
+        adjective_indices = self.strList_to_intList(word_indices[5])
+        # splits the user response array into separate arrays for each word type
+        user_responses = user_responses.split('|')
+        user_responses = [i.split(',') for i in user_responses]
+        replacement_nouns = user_responses[0]
+        replacement_verbs = user_responses[1]
+        replacement_adverbs = user_responses[2]
+        replacement_pnouns = user_responses[3]
+        replacement_pronouns = user_responses[4]
+        replacement_adjectives = user_responses[5]
+
+        for ind in range(len(noun_indices)):
+            mod_template[noun_indices[ind]] = re.sub(tag_re,replacement_nouns[ind],mod_template[noun_indices[ind]])
+
+        for ind in range(len(verb_indices)):
+            mod_template[verb_indices[ind]] = re.sub(tag_re,replacement_verbs[ind],mod_template[verb_indices[ind]])
             
-        for ind in range(len(self.adverb_indices)):
-            mod_template[self.adverb_indices[ind]] = re.sub(tag_re,self.replacement_adverbs[ind],mod_template[self.adverb_indices[ind]])
-        for ind in range(len(self.pnoun_indices)):
-            mod_template[self.pnoun_indices[ind]] = re.sub(tag_re,self.replacement_pnouns[ind],mod_template[self.pnoun_indices[ind]])
-        for ind in range(len(self.pronoun_indices)):
-            mod_template[self.pronoun_indices[ind]] = re.sub(tag_re,self.replacement_pronouns[ind],mod_template[self.pronoun_indices[ind]])
-        for ind in range(len(self.adjective_indices)):
-            mod_template[self.adjective_indices[ind]] = re.sub(tag_re,self.replacement_adjectives[ind],mod_template[self.adjective_indices[ind]])
+        for ind in range(len(adverb_indices)):
+            mod_template[adverb_indices[ind]] = re.sub(tag_re,replacement_adverbs[ind],mod_template[adverb_indices[ind]])
+
+        for ind in range(len(pnoun_indices)):
+            mod_template[pnoun_indices[ind]] = re.sub(tag_re,replacement_pnouns[ind],mod_template[pnoun_indices[ind]])
+
+        for ind in range(len(pronoun_indices)):
+            mod_template[pronoun_indices[ind]] = re.sub(tag_re,replacement_pronouns[ind],mod_template[pronoun_indices[ind]])
+
+        for ind in range(len(adjective_indices)):
+            mod_template[adjective_indices[ind]] = re.sub(tag_re,replacement_adjectives[ind],mod_template[adjective_indices[ind]])
         
         print(' '.join(mod_template))
 
@@ -113,3 +164,5 @@ class MLGen():
 
 genny = MLGen()
 genny.gen_template(sample_input)
+print(genny.make_DB_safe_counters())
+genny.pop_template(sample_input,full_sample,genny.safe_indices)
