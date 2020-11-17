e3f3ee5263d05194bcee2eb76f11da84b95e0325
KadeR-jpg <54875162+KadeR-jpg@users.noreply.github.com>
2020-11-17 15:39:30 -0700

    added to gitignore

--- djWebDev/djWebDev/settings.py
+++ djWebDev/djWebDev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#11_jadbr#@puda+6#d$)*)li^nxy*h%y)8*%0jpnip#b_v7wb'
+# SECRET_KEY = '#11_jadbr#@puda+6#d$)*)li^nxy*h%y)8*%0jpnip#b_v7wb'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', '#11_jadbr#@puda+6#d$)*)li^nxy*h%y)8*%0jpnip#b_v7wb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
